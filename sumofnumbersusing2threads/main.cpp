c6ba430bf99c9836c254105a09c5d1d686b2d891
= <=>
2025-11-22 08:46:52 +0000

    use 2 threads

--- sumofnumbersusing2threads/main.cpp
+++ sumofnumbersusing2threads/main.cpp
@@ -1,8 +1,9 @@
+#include <boost/thread.hpp>
 #include <boost/timer/timer.hpp>
 #include <cstdint>
 #include <iostream>
 
-int main() {
+void thread() {
   boost::timer::cpu_timer timer;
   std::uint64_t total = 0;
   for (int i = 0; i < 1'000'000'000; i++) {
@@ -10,6 +11,14 @@ int main() {
   }
   std::cout << timer.format() << std::endl;
   std::cout << total << std::endl;
+}
+
+int main() {
+  boost::thread t1{thread};
+  boost::thread t2{thread};
+
+  t1.join();
+  t2.join();
 
   return 0;
 }
