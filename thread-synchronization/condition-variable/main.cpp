bcccac43298f578ef7405f327a82287bd747bba8
= <=>
2025-11-23 15:15:52 +0000

    condition variable thread synchronization

---
+++ thread-synchronization/condition-variable/main.cpp
@@ -0,0 +1,40 @@
+#include <boost/thread.hpp>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <vector>
+
+boost::mutex mutex;
+boost::condition_variable_any cond;
+std::vector<int> random_numbers;
+void fill() {
+  random_numbers.reserve(3);
+  std::srand(static_cast<unsigned int>(std::time(0)));
+  for (size_t i = 0; i < 3; i++) {
+    boost::unique_lock<boost::mutex> lock{mutex};
+    random_numbers.push_back(std::rand());
+    cond.notify_all();
+    cond.wait(mutex);
+  }
+}
+
+void print() {
+  std::size_t next_size = 1;
+  for (size_t i = 0; i < 3; i++) {
+    boost::unique_lock<boost::mutex> lock{mutex};
+    while (random_numbers.size() != next_size)
+      cond.wait(mutex);
+    std::cout << random_numbers.back() << std::endl;
+    next_size++;
+    cond.notify_all();
+  }
+}
+
+int main() {
+  boost::thread t1{fill}, t2{print};
+
+  t1.join();
+  t2.join();
+
+  return 0;
+}
