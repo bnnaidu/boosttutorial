4e00df36d6fa3beecbbae7e6eaf307da34132dec
= <=>
2025-11-24 07:37:11 +0000

    scoped array of smart pointer

---
+++ chapter1/smartpointers/scopedarray/main.cpp
@@ -0,0 +1,11 @@
+#include <boost/scoped_array.hpp>
+#include <iostream>
+
+int main() {
+  boost::scoped_array<int> p{new int[2]};
+  *p.get() = 1;
+  p[1] = 2;
+  std::cout << p[1] << std::endl;
+  p.reset(new int[3]);
+  return 0;
+}
