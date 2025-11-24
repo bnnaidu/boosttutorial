6abd7c4e06df0c27a8aa7c6de12eb8679c5873b6
= <=>
2025-11-24 16:04:54 +0000

    make shared in shared pointer

---
+++ chapter1/smartpointers/makeshared/main.cpp
@@ -0,0 +1,12 @@
+#include <boost/make_shared.hpp>
+#include <iostream>
+#include <typeinfo>
+
+int main() {
+  auto p1 = boost::make_shared<int>(1);
+  std::cout << typeid(p1).name() << std::endl;
+  auto p2 = boost::make_shared<int[]>(10);
+  std::cout << typeid(p2).name() << std::endl;
+
+  return 0;
+}
