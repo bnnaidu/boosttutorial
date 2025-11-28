97b15352ee2f26f1f77db9402428ecd792d7fdb2
= <=>
2025-11-28 05:44:36 +0000

    modify elements in a container

---
+++ boostbind/modifyelementsinsidecontainer/main.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <boost/bind/bind.hpp>
+#include <boost/bind/placeholders.hpp>
+#include <iostream>
+#include <vector>
+
+void addAndPrint(int &x) { x += 10; }
+
+int main() {
+  std::vector<int> nums{1, 2, 3, 4, 5};
+  using namespace boost::placeholders;
+
+  std::for_each(nums.begin(), nums.end(), boost::bind(&addAndPrint, _1));
+
+  for (int x : nums)
+    std::cout << x << std::endl;
+
+  return 0;
+}
